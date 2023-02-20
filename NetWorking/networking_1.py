ec8c7e49c43612955037ad2f6be95cf23b83ea4d
Vlad <dogonovv@gmail.com>
2023-02-20 15:14:38 +0200

    минус лишняя строка

--- NetWorking/networking_1.py
+++ NetWorking/networking_1.py
@@ -4,9 +4,8 @@ import re
 sum = input('Введіть сумму в грн:  ')
 
 str = requests.get('https://api.monobank.ua/bank/currency')
-info = str.text
 
-pattern = re.findall(r'"currencyCodeA":840,"currencyCodeB":980,[\w:"]+,"rateBuy":([\d\s.]+)', info)
+pattern = re.findall(r'"currencyCodeA":840,"currencyCodeB":980,[\w:"]+,"rateBuy":([\d\s.]+)', str.text)
 
 result = float(sum)/float(pattern[0])
 print(f'Ви отримаете {round(result, 2)}$ по курсу {pattern[0]}')
