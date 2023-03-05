95a49bf97e64dd81bb9714cd554eb1d13bf1b939
Vlad <dogonovv@gmail.com>
2023-03-05 20:54:33 +0200

    Пробное решение двух задач по html+css

---
+++ bd/first.py
@@ -0,0 +1,13 @@
+import sqlite3 as sq
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+        name TEXT,
+        sex INTEGER DEFAULT 1,
+        old INTEGER,
+        score INTEGER
+    )
+    """)
+    # cur.execute("DROP TABLE users")
